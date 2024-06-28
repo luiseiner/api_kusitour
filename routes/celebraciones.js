d2d0e3f0ee9f0748b2dc8497633c44cbcefe78a5
luiseiner <117882180+luiseiner@users.noreply.github.com>
2024-06-27 23:00:46 -0500

    Add files via upload

---
+++ routes/celebraciones.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../config/db');
+const s3 = require('../config/s3');
+const multer = require('multer');
+const path = require('path');
+const upload = multer({ storage: multer.memoryStorage() });
+
+// Obtener todas las celebraciones
+router.get('/', (req, res) => {
+    db.query('SELECT * FROM celebraciones', (err, results) => {
+        if (err) {
+            console.error('Error fetching celebrations: ', err);
+            return res.status(500).send(err);
+        }
+        res.json(results);
+    });
+});
+
+
+
+module.exports = router;
